b7d6a15b44fe1dbf8da036dd5e2e064dafafd7cd
iltars <Ilya-Tar2003@yandex.ru>
2026-08-14 21:51:19 +0700

    mirroring

--- drone/run_env.sh
+++ drone/run_env.sh
@@ -2,8 +2,6 @@
 
 source /opt/ros/jazzy/setup.bash
 
-export PATH="/robotrio/drone/bin:${PATH}"
-
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/robotrio/drone/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/robotrio/drone/lib
 

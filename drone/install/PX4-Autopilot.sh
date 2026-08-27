e04111d58c9e99b93892cb671d4108eac15ed5b3
iltars <Ilya-Tar2003@yandex.ru>
2026-08-27 14:38:01 +0700

    not-build-px4

--- drone/install/PX4-Autopilot.sh
+++ drone/install/PX4-Autopilot.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-cd /tmp
+cd /
 
 git clone --recursive https://github.com/PX4/PX4-Autopilot.git
 
 cd PX4-Autopilot
-
-make px4_sitl -j$(nproc)
-
-mkdir -p /PX4-Autopilot
-
-cp -r /tmp/PX4-Autopilot/build/px4_sitl_default/bin /PX4-Autopilot/
-
-cp -r /tmp/PX4-Autopilot/build/px4_sitl_default/etc /PX4-Autopilot/
-
-cp -r /tmp/PX4-Autopilot/build/px4_sitl_default/ROMFS /PX4-Autopilot/
-
-cp -r /tmp/PX4-Autopilot/build/px4_sitl_default/rootfs /PX4-Autopilot/
-
-rm -rf /tmp/PX4-Autopilot

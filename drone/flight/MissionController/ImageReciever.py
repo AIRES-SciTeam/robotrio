42dba26886ea09fcb535dab682225caf87072d3f
iltars <Ilya-Tar2003@yandex.ru>
2026-09-21 01:58:19 +0700

    First_mission_blocks

--- drone/flight/MissionController/ImageReciever.py
+++ drone/flight/MissionController/ImageReciever.py
@@ -33,59 +33,26 @@ class DRONE_ImageReciever(Node):
 
         self.camera_params = None 
         self.dist_coeffs = None
-        self.dist_model = None
+
         self.frame = None
-        self.timestamp = None
-
-        self.fps = 0.0
-        self.fps_start = None
-        self.fps_intervals = 0
-        self.last_recieved_at = None
-
-        self.recieving_lag = None
 
     def _image_callback(self, msg : Image):
-        now = time.monotonic()
-        self.last_recieved_at = now
-
-        if self.fps_start is None:
-            self.fps_start = now
-        else:
-            self.fps_intervals += 1
-            elapsed = now - self.fps_start
-
-            if elapsed >= 1.0:
-                self.fps = self.fps_intervals / elapsed
-                self.fps_start = now
-                self.fps_intervals = 0
-
         try:
             frame = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         except CvBridgeError as e:
             self.logger.error(f"Image error: {e}")
             return
-
         self.frame = frame
-        self.timestamp = msg.header.stamp.sec * 1_000_000_000 + msg.header.stamp.nanosec
-
-        self.recieving_lag = time.monotonic() - now 
 
     def _camerainfo_callback(self, msg : CameraInfo):
         self.camera_params = (msg.k[0], msg.k[4], msg.k[2], msg.k[5])
         self.dist_coeffs = np.array(msg.d, dtype=np.float64)
-        self.dist_model = msg.distortion_model
         self.destroy_subscription(self.camerainfo_sub)
         self.camerainfo_sub = None
 
     def get_image(self):
-        return self.timestamp, self.frame
+        return self.frame
 
     def get_camerainfo(self):
-        return self.camera_params, self.dist_coeffs, self.dist_model
-
-    def get_fps(self, timeout=1.0):
-        if self.last_recieved_at is None:
-            return 0.0, self.recieving_lag
-        if time.monotonic() - self.last_recieved_at > timeout:
-            return 0.0, self.recieving_lag
-        return self.fps, self.recieving_lag
+        return self.camera_params, self.dist_coeffs
+    
--- drone/flight/MissionController/TagDetector.py
+++ drone/flight/MissionController/TagDetector.py
@@ -3,55 +3,40 @@ from pupil_apriltags import Detector
 import numpy as np
 import logging
 
-from drone.flight.Utils.Configs import DRONE_TagConfig
 
-
-class DRONE_TagHandler:
+class DRONE_TagDetector:
     def __init__(
         self, 
-        tag_config : DRONE_TagConfig,
+        tag_family : str,
         logger : logging.Logger
     ):
-        self.tag_config = tag_config
         self.logger = logger
-        
-        self.detector = Detector(families = tag_config.family)
+        self.detector = Detector(families = tag_family)
 
-        self.detections = dict()
-
-    def detect(self, timestamp, frame):
+    def detect(self, frame):
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        self.detections[timestamp] = self.detector.detect(
+        detections = self.detector.detect(
             gray_frame,
             estimate_tag_pose = False
         )
+        return detections
 
-        return [tag.tag_id for tag in self.detections[timestamp]]
-    
-    def estimate_pose(self, timestamp, tag_id, camera_params, dist_coeffs, tag_size):
-        if tag_size <= 0:
-            self.logger.error(f"Wrong tag size: {tag_size}")
-            raise ValueError(f"Wrong tag size: {tag_size}")
-        
-        detection = self.detections[timestamp].get(tag_id)
-        if detection is None:
-            return None
-
+    def estimate_pose(self, detection, camera_params, dist_coeffs, tag_size):
         _, corners = detection
-        fx, fy, cx, cy = camera_params
 
+        fx, fy, cx, cy = camera_params
         camera_matrix = np.array([
             [fx, 0, cx],
             [0, fy, cy],
-            [0, 0, 1] 
+            [0, 0, 1]
         ], dtype=np.float64)
 
-        half_tag_size = tag_size / 2
+        half = tag_size / 2
         object_points = np.array([
-            [-half_tag_size, half_tag_size, 0],
-            [half_tag_size, half_tag_size, 0],
-            [half_tag_size, -half_tag_size, 0],
-            [-half_tag_size, -half_tag_size, 0]
+            [-half, half, 0],
+            [half, half, 0],
+            [half, -half, 0],
+            [-half, -half, 0]
         ], dtype=np.float64)
 
         image_points = np.ascontiguousarray(corners, dtype=np.float64).reshape(4, 2)
@@ -66,7 +51,6 @@ class DRONE_TagHandler:
 
         if not success or not np.isfinite(tvec).all():
             return None
-
         if tvec[2, 0] <= 0:
             return None
 
@@ -74,3 +58,18 @@ class DRONE_TagHandler:
         rotation, _ = cv2.Rodrigues(rvec)
 
         return position, rotation
+
+    def get_tag_position(self, frame, tag_id, camera_params, dist_coeffs, tag_size):
+        detections = self.detect(frame)
+
+        det_idx : int | None = None
+        for idx, detection in enumerate(detections):
+            if detection.tag_id == tag_id:
+                det_idx = idx
+                break
+        else:
+            return None
+
+        pos, rot = self.estimate_pose(det_idx, camera_params, dist_coeffs, tag_size)
+
+        return pos, rot
